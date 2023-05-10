2906a0bcb5a90ba7852310aa1062f2513f5a0ac8
miraclekot <127429288+miraclekot@users.noreply.github.com>
2023-05-10 15:37:35 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+window.onload = function() {
+    lastnameInput = document.getElementById('lastname')
+    lastnameInput.addEventListener("input", cyrillicValidator, false);
+
+    
+    firstnameInput = document.getElementById('firstname')
+    firstnameInput.addEventListener("input", cyrillicValidator, false);
+
+    
+    surnameInput = document.getElementById('surname')
+    surnameInput.addEventListener("input", cyrillicValidator, false);
+}
+
+function cyrillicValidator() {
+    var res = /[^А-Яа-яЁё]/g.exec(this.value);
+    this.value = this.value.replace(res, '');
+}
+
+const value = document.querySelector("#value")
+const input = document.querySelector("#age")
+value.textContent = input.value
+input.addEventListener("input", (event) => {
+  value.textContent = event.target.value
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  var eventCalllback = function (e) {
+      var el = e.target,
+      clearVal = el.dataset.phoneClear,
+      pattern = el.dataset.phonePattern,
+      matrix_def = "+7(___) ___-__-__",
+      matrix = pattern ? pattern : matrix_def,
+      i = 0,
+      def = matrix.replace(/\D/g, ""),
+      val = e.target.value.replace(/\D/g, "");
+      if (clearVal !== 'false' && e.type === 'blur') {
+          if (val.length < matrix.match(/([\_\d])/g).length) {
+              e.target.value = '';
+              return;
+          }
+      }
+      if (def.length >= val.length) val = def;
+      e.target.value = matrix.replace(/./g, function (a) {
+          return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+      });
+  }
+  var phone_inputs = document.querySelectorAll('[data-phone-pattern]');
+  for (let elem of phone_inputs) {
+      for (let ev of ['input', 'blur', 'focus']) {
+          elem.addEventListener(ev, eventCalllback);
+      }
+  }
+});
